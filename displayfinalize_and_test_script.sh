1b21016beb8b6e366608ad7cce4ec0f7d5ab3932
stramazzo <37809964+stramazzo@users.noreply.github.com>
2018-03-26 23:02:37 +0200

    Update displayfinalize_and_test_script.sh

--- displayfinalize_and_test_script.sh
+++ displayfinalize_and_test_script.sh
@@ -6,7 +6,7 @@ sudo modprobe rp_usbdisplay
 #find fb
 cat /proc/fb | grep rpusbdisplay_fb
 #test display
-zcat /rp_usbdisplay/shoplogo.fb.gz > /dev/fb1
+zcat ./rp_usbdisplay/shoplogo.fb.gz > /dev/fb1
 
 #make the display autostart
 #sudo nano /etc/modules #aggiungere rp_usbdisplay come nuova riga al file
