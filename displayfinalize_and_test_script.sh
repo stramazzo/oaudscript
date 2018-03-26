737027ae30e2d2ed9515a85838b7ad0ddb6d4fb7
stramazzo <37809964+stramazzo@users.noreply.github.com>
2018-03-26 23:15:23 +0200

    Update displayfinalize_and_test_script.sh

--- displayfinalize_and_test_script.sh
+++ displayfinalize_and_test_script.sh
@@ -2,12 +2,21 @@
 #This script should finalize and test the usb display.
 
 #modprobe
+echo modprobe rp_usbdisplay
 sudo modprobe rp_usbdisplay
 #find fb
+echo finding fb
 cat /proc/fb | grep rpusbdisplay_fb
 #test display
+echo display test
 zcat ./rp_usbdisplay/shoplogo.fb.gz > /dev/fb1
 
 #make the display autostart
 #sudo nano /etc/modules #aggiungere rp_usbdisplay come nuova riga al file
-echo "rp_usbdisplay" >> /etc/modules
+if ['grep rp_usbdisplay /etc/modules']
+then
+  echo rp_usbdisplay alreay present in /etc/modules file.
+else
+  echo appending rp_usbdisplay to /etc/modules file.
+  echo "rp_usbdisplay" >> /etc/modules
+fi
